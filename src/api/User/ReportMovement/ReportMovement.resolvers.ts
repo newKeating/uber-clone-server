fad4e2ea46f4ea937bd31c7ebf8b8399712eedb9
Peter Kim <kroits09@gmail.com>
2019-04-07 13:35:52 +0900

    fix bug

--- src/api/User/ReportMovement/ReportMovement.resolvers.ts
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -1,30 +1,39 @@
-import { Resolvers } from "../../../types/resolvers";
-import privateResolver from "../../../utils/privateResolver";
-import { ReportMovementResponse, ReportMovementMutationArgs } from "../../../types/graph";
 import User from "../../../entities/User";
+import {
+  ReportMovementMutationArgs,
+  ReportMovementResponse
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
 import cleanNullArgs from "../../../utils/cleanNullArgs";
+import privateResolver from "../../../utils/privateResolver";
 
 const resolvers: Resolvers = {
   Mutation: {
-    ReportMovement: privateResolver(async (_, args: ReportMovementMutationArgs, {req, pubSub}): Promise<ReportMovementResponse> => {
-      const user: User = req.user
-      const notNull = cleanNullArgs(args)
-      try {
-        await User.update({id: user.id}, {...notNull})
-        const updatedUser = await User.findOne({ id: user.id });
-        pubSub.publish("driverUpdate", { DriversSubscription: updatedUser})
-        return {
-          ok: true,
-          error: null
-        }
-      } catch (error) {
-        return {
-          ok: false,
-          error: error.message[]
+    ReportMovement: privateResolver(
+      async (
+        _,
+        args: ReportMovementMutationArgs,
+        { req, pubSub }
+      ): Promise<ReportMovementResponse> => {
+        const user: User = req.user;
+        const notNull = cleanNullArgs(args);
+        try {
+          await User.update({ id: user.id }, { ...notNull });
+          const updatedUser = await User.findOne({ id: user.id });
+          pubSub.publish("driverUpdate", { DriversSubscription: updatedUser });
+          return {
+            ok: true,
+            error: null
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message
+          };
         }
       }
-    })
+    )
   }
 };
 
-export default resolvers
+export default resolvers;
